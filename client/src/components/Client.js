f3ae58bb8f8a3a2d5107f7a9e620a1187cc9388f
Jahidul Islam <56561463+sammijahidul@users.noreply.github.com>
2023-11-06 10:37:51 +0600

    now fetching clients info to the react app

--- client/src/components/Client.js
+++ client/src/components/Client.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { gql, useQuery} from '@apollo/client';
+import ClientRow from './ClientRow';
 
 const GET_CLIENTS = gql`
   query getClients {
@@ -19,7 +20,23 @@ const Client = () => {
     if (error) return <p>Something went wrong</p>
   return (
     <>
-      {!loading && !error && <h1>Client</h1>}
+      {!loading && !error && (
+        <table className='table table-hover mt-3'>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                {data.clients.map(client => (
+                    <ClientRow key={client.id} client={client} />
+                ))}
+            </tbody>
+        </table>
+      )}
     </>
   )
 }
